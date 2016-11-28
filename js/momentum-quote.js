8280788fcd3604fd30b97d1dc9c99c249744d0e9
Shuvo Habib <shuvohabib@gmail.com>
2016-11-28 13:21:24 +0600

    Add new author details

--- js/momentum-quote.js
+++ js/momentum-quote.js
@@ -4,16 +4,21 @@ window.momentum = window.momentum || {};
 
 // Quotes
 
-momentum.QuoteCtrl = function() {
-  this.apiUrl = "https://horizonshq.herokuapp.com/api/inspirationalquotes";
+momentum.QuoteCtrl = function () {
+    //this.apiUrl = "https://horizonshq.herokuapp.com/api/inspirationalquotes";
+    this.apiUrl = "https://andruxnet-random-famous-quotes.p.mashape.com/";
+
 };
 
 momentum.QuoteCtrl.prototype = {
-  fetchQuote: function(cb) {
-    $.ajax({
-      url: this.apiUrl,
-      method: "GET",
-      success: cb
-    });
-  }
+    fetchQuote: function (cb) {
+        $.ajax({
+            url: this.apiUrl,
+            headers: {
+                'X-Mashape-Key': 'v59o64xwMgmshgjqKwJypJ54sCR8p1ALpyijsnYauOzYVMbYRK'
+            },
+            method: "POST",
+            success: cb
+        });
+    }
 };
--- js/momentum.js
+++ js/momentum.js
@@ -2,37 +2,28 @@
 
 window.momentum = window.momentum || {};
 
-// Core - time, image
-
 momentum.Core = function () {
     this.timeStr = "";
     this.quoteStr = "";
+    this.quoteAuthorStr = "";
     this.weatherStr = "";
     this.ampm = "AM";
     this.salutation = "morning";
     this.location = "";
-
     this.timeEl = $("#time");
     this.quoteEl = $("#quote-text");
+    this.quoteAuthor = $("#quote-author");
     this.weatherEl = $("#weather");
     this.greetingEl = $("#greetings");
     this.ampmEl = $("#ampm");
     this.city = $("#location");
     this.lat;
     this.lon;
-
-    // weather controller
     this.weatherCtrl = new momentum.WeatherCtrl();
-
-    // quote controller
     this.quoteCtrl = new momentum.QuoteCtrl();
 };
 
 momentum.Core.prototype = {
-    // `setTime` method
-    // This method should calculate the current time and save it to timeStr in the form HH:MM, like: 12:01 or 21:34.
-    //
-    // hint. check out the `Date` object! Use `getHours` and `getMinutes`.
     setTime: function () {
         // YOUR CODE HERE
         var date = new Date();
@@ -58,66 +49,40 @@ momentum.Core.prototype = {
         ret = ret + hours + ':' + mins;
         this.timeStr = ret;
     },
-    // `setQuote` method
-    // This method should set the `quoteStr` property of the momentum core. This method will be used as the callback for quoteCtrl's `fetchQuote` function.
-    //
-    // hint. check out the `Date` object! Use `getHours` and `getMinutes`.
-    // hint. figure out what kind of response the quoteData is going to be, and see how you might be able to access the quote of the day from that.
     setQuote: function (quoteData) {
-        this.quoteStr = quoteData.message;
+        this.quoteStr = JSON.parse(quoteData).quote;
+        this.quoteAuthorStr = JSON.parse(quoteData).author;
         this.quoteEl.text(this.quoteStr);
+        this.quoteAuthor.text(this.quoteAuthorStr);
         this.render();
+        console.log(quoteData);
     },
-    // `setWeather` method
-    // This method should set the `weatherStr` property of the momentum core. This method will be used as the callback for weatherCtrl's `fetchWeather` function.
-    //
-    // hint. figure out what kind of response the weatherData is going to be, and see how you might be able to access the quote of the day from that.
     setWeather: function (weatherData) {
-        // YOUR CODE HERE
         this.weatherStr = Math.floor(weatherData.main.temp - 273.15);
         this.location = weatherData.name;
         this.render();
     },
-    // `updateTime` method
-    // This function should call setTime() so that this.timeStr is updated.
     updateTime: function () {
-        // YOUR CODE HERE
         this.setTime();
     },
-    // `updateWeather` method
-    // This function should call weatherCtrl.fetchWeather and pass in this.setWeather as the callback.
-    //
-    // note. you might run into scoping issues again. You should know how to solve them by now, using .call, .apply, or .bind.
     updateWeather: function () {
-        // YOUR CODE HERE
         this.weatherCtrl.fetchWeather(this.lat, this.lon, this.setWeather.bind(this));
     },
-    // `updateQuote` method
-    // This function should call quoteCtrl.fetchQuote and pass in this.setQuote as the callback.
-    //
-    // note. you might run into scoping issues again. You should know how to solve them by now, using .call, .apply, or .bind.
     updateQuote: function () {
-        // YOUR CODE HERE
         this.quoteCtrl.fetchQuote(this.setQuote.bind(this));
 
     },
-    // `start` method
-    // This method will call some of the `update...` methods. This function will be called when the page has finished loading, so that Momentum can start off with the more up-to-date data.
     start: function () {
-        // get location
         if (!navigator.geolocation) {
             throw "Geolocation not supported!";
         }
-
         function error() {
             throw "Error occured!";
         };
-
         navigator.geolocation.getCurrentPosition(function (position) {
-            console.log("EXECUTING");
             this.lat = position.coords.latitude;
             this.lon = position.coords.longitude;
-            console.log(this.lat, this.lon);
+            //console.log(this.lat, this.lon);
             this.updateWeather();
         }.bind(this), error);
         this.setTime();
@@ -127,16 +92,13 @@ momentum.Core.prototype = {
 
 
     },
-    // `render` method
-    // This method should "render" the time, quote and weather strings on your page by replacing the text value of your elements with their respective properties.
-    // ex. this.timeStr will be rendered on to the screen using this.timeEl.text(this.timeStr);
     render: function () {
-        // YOUR CODE HERE
         this.timeEl.text(this.timeStr);
         this.greetingEl.text("Good " + this.salutation + ", Shuvo Habib");
         this.ampmEl.text(this.ampm);
         this.weatherEl.text(this.weatherStr);
         this.quoteEl.text(this.quoteStr);
+        this.quoteAuthor.text(this.quoteAuthorStr);
         this.city.text(this.location);
     }
 };
